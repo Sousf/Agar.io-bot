9997d49581ecc728e5b84b33479cf71621b53c24
Sousf <vopeter1662000@gmail.com>
2021-07-10 03:27:43 +1000

    Attempted cell movement control base on mouse pos

--- main.py
+++ main.py
@@ -14,27 +14,50 @@ def pressPlay():
 
 def split():
     # Press space in the direction of mouse cursor
+    pyautogui.click('space')
     
     pass
 
+
+# TODO: fix cell movement base on mouse position
 def img_grab():
     # Grab pixel colour on screen
     # RGB Colour of the splitter is (51, 255, 51)
 
     # the box for grab() is set to None to capture the entire monitor screen as an image.
     screenImage = ImageGrab.grab(None)
+    splitter_colour = (51, 255, 51)
     print(screenImage)
+
      # Grab the colour of the green splitter (No longer required)
-    splitter_colour = screenImage.getpixel((1720,1087))
-    print(splitter_colour)
+    # splitter_colour = screenImage.getpixel((1720,1087))
+    # print(splitter_colour)
+
+    splitter_found = False
+    for x in range(0,2560):
+        for y in range(0,1440):
+            if (screenImage.getpixel((x,y)) == splitter_colour):
+                moveMouseCursor(x,y)
+                splitter_found = True
+                break
+        if (splitter_found):
+            # splitter_found == False
+            return splitter_found
 
 
 
 
-
-def moveMouseCursor():
-    pass
+def moveMouseCursor(x,y):
+    pyautogui.moveTo(x, y)
 
 # time.sleep(4)
 # pressPlay()
-img_grab()
+time.sleep(4)
+pressPlay()
+while True:
+    # time.sleep(4)
+    img_grab()
+    # stop_condition = img_grab()
+    # if (stop_condition):
+    #     break
+    
